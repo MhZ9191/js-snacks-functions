648c0ae0a50d87a3b2772d48972aa1050f4977c7
MhZ9191 <hegic91@gmail.com>
2026-01-09 14:30:10 +0100

    Added 01_saluti

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,24 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
-
+const userName = "Mario";
 
 // Dichiara la funzione qui.
 
+//! prima versione
+//* const greet = (name) => console.log(`ciao ${name}`);
+
+const greet = (name) => {
+  const union = "ciao " + name;
+  return union;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 
+//! prima versione
+//* greet("mario");
 
+const salutaMario = greet("Mario");
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+
+console.log(salutaMario);
