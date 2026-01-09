3513e4adf1351d5eb5b764120b991f960ae1a4c6
MhZ9191 <hegic91@gmail.com>
2026-01-09 15:10:22 +0100

    Added 04_conta_vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,28 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
 
+const countVow = (exStr) => {
+  const vocali = ["a", "e", "i", "o", "u"];
+  let count = 0;
+
+  for (let i = 0; i < exStr.length; i++) {
+    if (vocali.includes(exStr[i])) count++;
+    //oppure ciclo for
+    // for (let m = 0; m < vocali.length; m++) {
+    //   if (exStr[i] === vocali[m]) count++;
+    // }
+  }
+
+  return count;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const tryVocali = countVow(word);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+console.log(tryVocali);
