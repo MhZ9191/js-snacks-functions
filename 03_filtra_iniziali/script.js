70e93f34e48330573563b1542b01c5b82c07e042
MhZ9191 <hegic91@gmail.com>
2026-01-09 14:50:39 +0100

    Added 03_filtra_iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,23 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+const cArr = (exArr, char) => {
+  const funArr = [];
+  for (let i = 0; i < exArr.length; i++) {
+    if (exArr[i].charAt(0) === char) {
+      funArr.push(exArr[i]);
+    }
+  }
+
+  return funArr;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const tryFunction = cArr(names, "A");
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
+console.log(tryFunction);
